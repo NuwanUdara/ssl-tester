ed909487f321c167e2e502618f441a858b42b491
nuwanudara <nuwanudara36@gmail.com>
2023-05-11 09:39:24 +0530

    added another end point

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const req = require('express/lib/request');
 const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!').statusCode(200);
+})
+
+app.get('/test', (req,res)=> {
+    res.send('end point reached!').statusCode(200)
 })
 
 app.listen(port, () => {
