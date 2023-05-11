9b480fbe4fcbf2b346d13b52f3e4a55e6af6c307
nuwanudara <nuwanudara36@gmail.com>
2023-05-11 09:52:19 +0530

    port to 443

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const req = require('express/lib/request');
 const app = express()
-const port = 80
+const port = 443
 
 app.get('/', (req, res) => {
   res.send('Hello World!').statusCode(200);
