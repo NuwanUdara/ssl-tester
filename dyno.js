a1ca9d33fe1296ea0454e312b127211c0d537eb0
nuwanudara <nuwanudara36@gmail.com>
2023-05-12 15:36:02 +0530

    fixed the schema

--- dyno.js
+++ dyno.js
@@ -27,9 +27,9 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient()
 
 dynamoDB
   .get({
-    TableName: "cert",
+    TableName: "certs",
     Key: {
-      id: "test1",
+      api: "test1",
     },
   })
   .promise()
