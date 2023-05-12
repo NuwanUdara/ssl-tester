25f72031f3091d7750af7b8ffbc7419698d71fdf
nuwanudara <nuwanudara36@gmail.com>
2023-05-12 15:37:10 +0530

    removed non working codes

--- dyno.js
+++ dyno.js
@@ -1,26 +1,3 @@
-// var aws = require('aws-sdk');
-// var db = new aws.DynamoDB({
-//   region: 'us-west-2',
-//   maxRetries: 1,
-//   accessKeyId: 'ASIAXISCRMT26P3W6ENC',
-//   secretAccessKey: 'FyTCOwrSVXMYaR0wvtS+/QwugJQSnlwPZrdz4Egy',
-// });
-
-// const queryMyThings = async (event) => { 
-//  var params = {
-//       Key: {
-//       "api": {
-//          S: "test1"
-//         }
-//       }, 
-//       TableName: "cert"
-//      };
-
-//     const data = await db.getItem(params).promise();
-
-// }
-// console.log(queryMyThings())
-
 const AWS = require("aws-sdk")
 AWS.config.update({ region: "us-west-2" })
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
